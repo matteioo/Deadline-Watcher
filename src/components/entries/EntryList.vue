c6bd0491c74bd10695d02e0393d7e4b784c54144
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2022-07-28 20:39:55 +0200

    styling the sections of the list

--- src/components/entries/EntryList.vue
+++ src/components/entries/EntryList.vue
@@ -18,13 +18,15 @@ export default {
 
 <template>
 	<div v-if="entries.length > 0">
-		<div class="flex flex-row justify-between items-center mx-4 [@supports(backdrop-filter:blur(0))]:mx-0 [@supports(backdrop-filter:blur(0))]:px-4 sm:text-lg font-medium font-sans-rounded text-gray-100 sticky top-0 [@supports(backdrop-filter:blur(0))]:bg-gray-900/30 bg-gray-900/70 -mt-2 pt-2 -mb-1 pb-1 border-t border-gray-800 backdrop-blur-sm">
-			<h2 class="uppercase">{{ title }}</h2>
-			<Badge :count="entries.length"/>
-		</div>
-		<div class="sm:my-3">
-			<div v-for="entry in entries">
-				<Entry :title="entry.title" :category="entry.lva" :date-time="entry.date"/>
+		<div>
+			<div class="flex flex-row justify-between items-center mx-4 [@supports(backdrop-filter:blur(0))]:mx-0 [@supports(backdrop-filter:blur(0))]:px-4 sm:text-lg font-medium font-sans-rounded text-gray-100 sticky top-0 [@supports(backdrop-filter:blur(0))]:bg-gray-900/30 bg-gray-900/70 -mt-2 pt-2 -mb-1 pb-1 border-t border-gray-800 backdrop-blur-sm">
+				<h2 class="uppercase">{{ title }}</h2>
+				<Badge :count="entries.length"/>
+			</div>
+			<div class="mb-4 pb-2 sm:pb-6">
+				<div v-for="entry in entries">
+					<Entry :title="entry.title" :category="entry.lva" :date-time="entry.date"/>
+				</div>
 			</div>
 		</div>
 	</div>
